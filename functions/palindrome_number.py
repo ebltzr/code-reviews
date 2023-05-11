8bdcf38a5626a94f4eab14368201f35ec88c98fe
Yulia S <startyupp@gmail.com>
2023-05-11 14:19:16 -0700

    Created a palindrome number function. Needs to get bug fixed.

--- functions/palindrome_number.py
+++ functions/palindrome_number.py
@@ -1,2 +1,9 @@
-def palindrome_number(x):
-    pass
+def palindrome_number(number):
+    str_num = str(number)
+    end_index = len(str_num) - 1
+    for i in range(len(str_num)//2):
+        str_num[i] == str_num[end_index]
+        end_index -= 1
+        
+assert palindrome_number(123321) == True
+assert palindrome_number(123322) == False
